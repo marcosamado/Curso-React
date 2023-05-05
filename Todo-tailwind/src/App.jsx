761abd07707768840733e78e1773b8944eba46ec
marcosamado <led_9dejulio@hotmail.com>
2023-05-04 22:32:11 -0300

    empezando todo con tailwind

--- Todo-tailwind/src/App.jsx
+++ Todo-tailwind/src/App.jsx
@@ -1,9 +1,48 @@
 const App = () => {
+    return (
+        <>
+            <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
+                <header className="container mx-auto px-4 pt-8">
+                    <div className="flex justify-between">
+                        <h1 className="text-3xl font-semibold uppercase tracking-[0.4em] text-white">
+                            Todo
+                        </h1>
+                        <button>Luna</button>
+                    </div>
+                    <form className="bg-white rounded-md overflow-hidden py-4">
+                        <input type="text" placeholder="Create a new todo..." />
+                    </form>
+                </header>
+                <main className="container mx-auto px-4">
+                    <article>
+                        <button>circulo</button>
+                        <p>Complete online JavaScript Curse</p>
+                        <button>X</button>
+                    </article>
+                    <article>
+                        <button>circulo</button>
+                        <p>Complete online JavaScript Curse</p>
+                        <button>X</button>
+                    </article>
+                    <article>
+                        <button>circulo</button>
+                        <p>Complete online JavaScript Curse</p>
+                        <button>X</button>
+                    </article>
+                    <section>
+                        <span>5 items left</span>
+                        <button>Clear Completed</button>
+                    </section>
+                </main>
 
-  return (
-    <h1 className="text-center">Holis</h1>
-  );
+                <section className="container mx-auto px-4">
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </section>
+            </div>
+        </>
+    );
 };
-
 
 export default App;
