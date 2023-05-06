45bd09dfc7494a667d9b8a3bf02021cbeea9313b
marcosamado <led_9dejulio@hotmail.com>
2023-05-05 21:55:20 -0300

    trabajando en la estructura de la todo-tailwind

--- Todo-tailwind/src/App.jsx
+++ Todo-tailwind/src/App.jsx
@@ -1,33 +1,46 @@
+import CrossIcon from "./components/icons/CrossIcon";
+
 const App = () => {
     return (
         <>
-            <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
-                <header className="container mx-auto px-4 pt-8">
+            <div className="min-h-screen bg-gray-200 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
+                <header className="container mx-auto px-6 pt-8">
                     <div className="flex justify-between">
-                        <h1 className="text-3xl font-semibold uppercase tracking-[0.4em] text-white">
+                        <h1 className=" text-3xl font-semibold uppercase tracking-[0.4em] text-white">
                             Todo
                         </h1>
-                        <button>Luna</button>
+                        <button className="">Luna</button>
                     </div>
-                    <form className="bg-white rounded-md overflow-hidden py-4">
-                        <input type="text" placeholder="Create a new todo..." />
+                    <form className="mb-5 mt-8 flex items-center gap-2 overflow-hidden rounded-md bg-white px-5 py-3.5">
+                        <span className=" inline-block h-5 w-5 rounded-full border-2"></span>
+                        <input
+                            className="w-full text-gray-500 outline-none"
+                            type="text"
+                            placeholder="Create a new todo..."
+                        />
                     </form>
                 </header>
-                <main className="container mx-auto px-4">
-                    <article>
-                        <button>circulo</button>
-                        <p>Complete online JavaScript Curse</p>
-                        <button>X</button>
+                <main className="container mx-auto px-6">
+                    <article className="flex gap-2 rounded-md border-2 bg-white px-4 py-3.5">
+                        <button className="h-5 w-5 flex-none rounded-full border-2"></button>
+                        <p className="grow pt-0.5 text-xs font-bold text-gray-600">
+                            Complete online JavaScript Curse
+                        </p>
+                        <button className="flex-none">{<CrossIcon />}</button>
                     </article>
-                    <article>
-                        <button>circulo</button>
-                        <p>Complete online JavaScript Curse</p>
-                        <button>X</button>
+                    <article className="flex justify-between rounded-md border-2 bg-white px-4 py-3.5">
+                        <button className="h-5 w-5 rounded-full border-2"></button>
+                        <p className="w-auto pt-0.5 text-xs font-bold text-gray-600">
+                            Complete online JavaScript Curse
+                        </p>
+                        <button>{<CrossIcon />}</button>
                     </article>
-                    <article>
-                        <button>circulo</button>
-                        <p>Complete online JavaScript Curse</p>
-                        <button>X</button>
+                    <article className="flex justify-between rounded-md border-2 bg-white px-4 py-3.5">
+                        <button className="h-5 w-5 rounded-full border-2"></button>
+                        <p className="w-auto pt-0.5 text-xs font-bold text-gray-600">
+                            Complete online JavaScript Curse
+                        </p>
+                        <button>{<CrossIcon />}</button>
                     </article>
                     <section>
                         <span>5 items left</span>
