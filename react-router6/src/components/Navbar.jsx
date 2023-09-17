a1118202db864d0741cf7f4cea332bb7ef3c0ff5
marcosamado <led_9dejulio@hotmail.com>
2023-09-16 21:04:13 -0300

    practicando react-router

---
+++ react-router6/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+const Navbar = () => {
+    return (
+        <div className="navbar navbar-dark bg-dark">
+            <div className="container">
+                <a href="/" className="btn btn-outline-primary">
+                    Inicio
+                </a>
+                <a href="/" className="btn btn-outline-primary">
+                    Nosotros
+                </a>
+                <a href="/" className="btn btn-outline-primary">
+                    Contacto
+                </a>
+                <a href="/" className="btn btn-outline-primary">
+                    Blog
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
